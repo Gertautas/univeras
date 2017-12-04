fd5b923a6b6e56c983ad8d8dc38a48805ba40333
Gertautas <gmaciuta@gmail.com>
2017-12-04 16:15:22 +0200

    7lab neveikia notification

--- 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/Main2Activity.java
+++ 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/Main2Activity.java
@@ -1,16 +1,21 @@
 package com.example.gertautasm.a7laboras;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
 import android.os.BatteryManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +23,7 @@ import android.widget.Toast;
 public class Main2Activity extends AppCompatActivity {
 
     private BroadcastReceiver battery = new BroadcastReceiver() {
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
         @Override
         public void onReceive(Context context, Intent intent) {
             int level = intent.getIntExtra("level",0);
@@ -25,10 +31,10 @@ public class Main2Activity extends AppCompatActivity {
             pb.setProgress(level);
             TextView textView = (TextView) findViewById(R.id.tekstas);
             textView.setText("Battery level: " + Integer.toString(level) + "%");
-            if(level< 25){
+            if(level< 25 & level >= 16){
                 Toast.makeText(context,"Baterija senka",Toast.LENGTH_LONG).show();
-            }else if(level <=15){
-                notification();
+            }else if(level <= 15){
+                createNotification();
             }
 
         }
@@ -41,15 +47,17 @@ public class Main2Activity extends AppCompatActivity {
 
         registerReceiver(battery,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
-    public void notification(){
-        NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(Main2Activity.this)
-                .setSmallIcon(android.R.drawable.stat_notify_error)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
-                .setContentTitle("Low battery")
-                .setContentText("Your battery is below 15% ");
-        notBuilder.setDefaults(
-                Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
-        NotificationManagerCompat notificationmanager = NotificationManagerCompat.from(Main2Activity.this);
-        notificationmanager.notify(1,notBuilder.build());
+    public void createNotification(){
+      NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+              .setSmallIcon(R.drawable.icon)
+              .setContentTitle("Low Battery")
+              .setContentText("Your batter level is below 15% ");
+
+      Intent intent = new Intent(this,Main2Activity.class);
+      PendingIntent content = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+      builder.setContentIntent(content);
+
+      NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+      manager.notify(0,builder.build());
     }
 }
