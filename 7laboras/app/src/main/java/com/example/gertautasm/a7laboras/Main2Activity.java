95aa64e55bc9f42e26bc7138b7c94c6ca06b8480
Gertautas <gmaciuta@gmail.com>
2017-12-06 10:18:19 +0200

    nauji failai

--- 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/Main2Activity.java
+++ 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/Main2Activity.java
@@ -1,105 +1,30 @@
 package com.example.gertautasm.a7laboras;
 
-import android.app.Notification;
+
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
-import android.os.BatteryManager;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
 
-    int mnotificationId = 001;
-    String CHANNEL_ID = "my_channel_01";
-
-    private BroadcastReceiver battery = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra("level", 0);
-            ProgressBar pb = (ProgressBar) findViewById(R.id.baras);
-            pb.setProgress(level);
-            TextView textView = (TextView) findViewById(R.id.tekstas);
-            textView.setText("Battery level: " + Integer.toString(level) + "%");
-            if (level < 25 & level >= 16) {
-                Toast.makeText(context, "Baterija senka", Toast.LENGTH_LONG).show();
-            } else if (level <= 15) {
-                
-                Notification.Builder not;
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    not = new Notification.Builder(context,"text");
-                } else {
-                    not = new Notification.Builder(context);
-                }
-
-                not.setSmallIcon(android.R.drawable.stat_notify_error);
-                not.setAutoCancel(true);
-                not.setContentTitle("Baterija miršta");
-                not.setContentText("Spausti čia!");
-
-                Intent intents = new Intent(context, Main2Activity.class);
-
-                PendingIntent pi = PendingIntent.getActivity(context,1,intents, PendingIntent.FLAG_CANCEL_CURRENT);
-                not.setContentIntent(pi);
-
-                NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    nm.notify(1, not.build());
-                }
-            }
-
-        }
-    };
+public class Main2Activity extends BroadcastReceiver {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-        registerReceiver(battery, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    public void onReceive(Context context, Intent intent) {
+            createNotification(context);
     }
+    public void createNotification(Context context){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.icon)
+                .setContentTitle("Low Battery")
+                .setContentText("Your batter level is below 15% ");
 
-    public void sendNotification() {
+        PendingIntent content = PendingIntent.getActivity(context,0,new Intent(context,MainActivity.class),0);
+        builder.setContentIntent(content);
 
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0,builder.build());
     }
 }
-
-
-//
-//    public void onReceive(Context context, Intent intent) {
-//        Notification.Builder not;
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            not = new Notification.Builder(context,"text");
-//        } else {
-//            not = new Notification.Builder(context);
-//        }
-//
-//        not.setSmallIcon(android.R.drawable.stat_notify_error);
-//        not.setAutoCancel(true);
-//        not.setContentTitle("Baterija miršta");
-//        not.setContentText("Spausti čia!");
-//
-//        Intent intents = new Intent(context, MainActivity.class);
-//
-//        PendingIntent pi = PendingIntent.getActivity(context,1,intents, PendingIntent.FLAG_CANCEL_CURRENT);
-//        not.setContentIntent(pi);
-//
-//        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-//            nm.notify(1, not.build());
-//        }
-//    }
--- 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/MainActivity.java
+++ 7laboras/app/src/main/java/com/example/gertautasm/a7laboras/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.gertautasm.a7laboras;
 
-import android.app.Notification;
-import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int level;
+    Main2Activity main = new Main2Activity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,28 @@ public class MainActivity extends AppCompatActivity {
         pradeti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                registerReceiver(battery,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
             }
         });
+
+    }
+    private BroadcastReceiver battery = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            pb(intent);
+            if(level< 25 & level >= 16){
+                Toast.makeText(context,"Baterija senka",Toast.LENGTH_LONG).show();
+            }else if(level <= 15){
+                main.onReceive();
+            }
+        }
+    };
+    public void pb(Intent intent){
+        level = intent.getIntExtra("level",0);
+        ProgressBar pb = (ProgressBar) findViewById(R.id.baras);
+        pb.setProgress(level);
+        TextView textView = (TextView) findViewById(R.id.tekstas);
+        textView.setText("Battery level: " + Integer.toString(level) + "%");
     }
 }
